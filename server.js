750b6433cbc82566539e6b33da1bc88e5f0e8dcd
Danil <42476651+udlekov@users.noreply.github.com>
2018-08-18 05:45:06 +0000

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-//NodeJS script for create local server
-var http = require("http");
-
-http.createServer(function(request,response){
-	response.end();
-}).listen(8080, "127.0.0.1", function(){
-	console.log("Сервер начал работу");
-});
